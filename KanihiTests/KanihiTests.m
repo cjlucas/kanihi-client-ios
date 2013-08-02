a9296930d86ca5ff6db0fc3f3320b3655ab76aa2
Chris Lucas <chris@chrisjlucas.com>
2013-08-02 12:23:39 -0700

    [tests] added data store tests

--- KanihiTests/KanihiTests.m
+++ KanihiTests/KanihiTests.m
@@ -8,17 +8,64 @@
 
 #import <XCTest/XCTest.h>
 
+#import "KANDataStore.h"
+
+#import "KANConstants.h"
+
+@interface KANMockDataStore : KANDataStore
+
+- (NSManagedObjectModel *)managedObjectModel;
+
+- (void)handleTrackDatas:(NSArray *)trackDatas;
+
+@end
+
+@implementation KANMockDataStore
+
+- (NSManagedObjectModel *)managedObjectModel
+{
+    static NSManagedObjectModel *_managedObjectModel;
+
+    if (_managedObjectModel == nil) {
+        NSURL * modelURL = [[NSBundle mainBundle] URLForResource:@"Model" withExtension:@"momd"];
+        _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+    }
+
+    return _managedObjectModel;
+}
+
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
+{
+    static NSPersistentStoreCoordinator *_psc = nil;
+    if (!_psc) {
+        _psc = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
+        [_psc addPersistentStoreWithType:NSInMemoryStoreType configuration:nil URL:[NSURL URLWithString:@"memory://datastore"] options:nil error:nil];
+    }
+
+    return _psc;
+}
+
+@end
+
 @interface KanihiTests : XCTestCase
 
+@property (readonly) KANMockDataStore *dataStore;
+@property NSArray *initialData;
 @end
 
 @implementation KanihiTests
 
+@synthesize dataStore = _dataStore;
+
 - (void)setUp
 {
     [super setUp];
-    
-    // Set-up code here.
+    _dataStore = [[KANMockDataStore alloc] init];
+
+    NSBundle *testBundle = [NSBundle bundleForClass:[self class]];
+
+    NSURL *tracks1 = [testBundle URLForResource:@"tracks1" withExtension:@"json"];
+    self.initialData = [NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfURL:tracks1] options:0 error:nil];
 }
 
 - (void)tearDown
@@ -28,9 +75,26 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)importInitialData
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    [self.dataStore handleTrackDatas:self.initialData];
+}
+
+- (NSArray *)fetchAllEntriesWithEntity:(NSString *)entityName
+{
+    NSFetchRequest *req = [NSFetchRequest fetchRequestWithEntityName:entityName];
+    return [self.dataStore.mainManagedObjectContext executeFetchRequest:req error:nil];
+}
+
+# pragma mark - Test Cases
+
+- (void)testImportData
+{
+    [self importInitialData];
+
+    NSArray *results = [self fetchAllEntriesWithEntity:KANTrackEntityName];
+
+    XCTAssertTrue(results.count == 10);
 }
 
 @end
